118d3d14ea70622490e64f86d72f998b611d5348
Noël Camps <raamschoon@gmail.com>
2025-02-17 15:34:36 +0100

    update 3 sport app

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// Deze file wordt gebruikt voor extra functionaliteit zoals voortgang, knoppen die dynamisch werken, etc.
+// Als je een back-end wilt toevoegen (zoals NoSQL), kun je deze code uitbreiden voor servercommunicatie.
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Hier kunnen we extra functionaliteit toevoegen als we willen.
+});
